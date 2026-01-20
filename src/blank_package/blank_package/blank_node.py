8fb974c8aa34697dd4231af0b845c357de32876c
Mariza Paspalli <mariza@Paspallis-MacBook-Air.local>
2026-01-20 14:07:33 +0200

    Copied example code

--- src/blank_package/blank_package/blank_node.py
+++ src/blank_package/blank_package/blank_node.py
@@ -2,25 +2,41 @@
 import os
 import rclpy
 from rclpy.node import Node
+from duckietown_msgs.msg import LEDPattern
+from std_msgs.msg import ColorRGBA
 
-# Fill in something for msg type imports
-# from duckietown_msgs.msg import SOMETHING
-# from std_msgs.msg import SOMETHING
 
-class SkeletonNode(Node):
+class Blinker(Node):
     def __init__(self):
-        super().__init__('blank_node')
-        #Create publishers and subscribers in init, use callback
-        pass
-    
-    #Define callback functions here
+        super().__init__('blinker')
+        self.vehicle_name = os.getenv('VEHICLE_NAME')
+        self.publisher = self.create_publisher(LEDPattern, f'/{self.vehicle_name}/led_pattern', 1)
+        self.counter = 0
+        self.timer = self.create_timer(1, self.publish_pattern)
+
+    def publish_pattern(self):
+        # LEDPattern is a custom Duckietown Message
+        msg = LEDPattern()
+
+        if self.counter % 3 == 0:
+            # ColorRGBA is a standard message of ROS2
+            pattern = ColorRGBA(r=1.0, g=0.0, b=0.0, a=1.0)
+        elif self.counter % 3 == 1:
+            pattern = ColorRGBA(r=0.0, g=1.0, b=0.0, a=1.0)
+        else:
+            pattern = ColorRGBA(r=0.0, g=0.0, b=1.0, a=1.0)
+
+        # 5 Leds to fill
+        msg.rgb_vals = [pattern] * 5
+
+        self.publisher.publish(msg)
+        self.counter += 1
 
 
 def main():
-    print('In main')
     rclpy.init()
-    node = SkeletonNode()
-    rclpy.spin(node)
+    blinker = Blinker()
+    rclpy.spin(blinker)
     rclpy.shutdown()
 
 
